ce7c93c0a3ded45896904cd4d10b3814d382b04f
agbaigospel2018-source <agbaigospel2018@gmail.com>
2026-07-17 20:58:27 +0100

    migration issue fixes

--- belleful_express/settings.py
+++ belleful_express/settings.py
@@ -81,13 +81,23 @@ WSGI_APPLICATION = 'belleful_express.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.parse(
-        os.environ.get('DATABASE_URL'),
-        conn_max_age=600,
-        conn_health_checks=True,
-    )
-}
+DATABASE_URL = os.environ.get('DATABASE_URL')
+
+if DATABASE_URL:
+    DATABASES = {
+        'default': dj_database_url.parse(
+            DATABASE_URL,
+            conn_max_age=600,
+            conn_health_checks=True,
+        )
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 
