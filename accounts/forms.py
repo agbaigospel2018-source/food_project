6bb6955350a39b0b85204819aa3fbf803afcf825
agbaigospel2018-source <agbaigospel2018@gmail.com>
2026-07-23 23:49:48 +0100

    register.html changes

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,3 +1,4 @@
+
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
@@ -18,7 +19,16 @@ class RegisterForm(UserCreationForm):
             'password2',
         )
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
+        for field in self.fields.values():
+            field.widget.attrs.update({
+                'class': 'w-full border border-gray-300 rounded-lg px-4 py-3 focus:outline-none focus:ring-2 focus:ring-orange-500'
+            })
+
+        
+        
 class ProfileUpdateForm(forms.ModelForm):
 
     class Meta:
